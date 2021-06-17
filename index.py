cc109eda9076bd0549ecb70c7e4f5b97b20a8c75
Brandon Harris <brandonharris177@gmail.com>
2021-06-17 09:36:23 -0600

    chessKnightMoves

--- index.py
+++ index.py
@@ -2416,3 +2416,24 @@ def bishopAndPawn(bishop, pawn):
     if abs(int(pawn[1]) - int(bishop[1])) == abs(alphabet[pawn[0]] - alphabet[bishop[0]]):
         return True
     return False
+
+def chessKnightMoves(cell):
+    moveCount = 0
+    dictionary = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8
+    }
+    moves = [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]]
+    horizontal = dictionary[cell[0]]
+    vertical = int(cell[1])
+    for move in moves:
+        if 0 < horizontal + move[0] < 9 and 0 < vertical + move[1] < 9:
+            moveCount+=1
+            
+    return moveCount
